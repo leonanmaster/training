0eee53694dae1d224a2e072f923199ae08de6973
Leonan <leonanlouvem2015@gmail.com>
2025-10-03 12:26:27 -0300

    flowers passou

--- CF/flowers.cpp
+++ CF/flowers.cpp
@@ -1,21 +1,21 @@
-    #include <bits/stdc++.h>
+#include <bits/stdc++.h>
     using namespace std;
      
     int main()
     {
-        long long t, k ;
+        int t, k ;
         cin >> t;
         cin >> k;
      
-        vector<long long> dp; // guardará as combinações para cad a i
-        
+        vector<int> dp; // guardará as combinações para cad a i
+       
         //prefix sum
-        vector<long long> ps; // guardará as somas para n precisar ter um for q calcula as somas dos intervalos 
-        
+        vector<int> ps; // guardará as somas para n precisar ter um for q calcula as somas dos intervalos
+       
         ps.push_back(0);
      
         // nesse passo é fundamental estabelecer a RR de dp (dp[i] = dp[i-1] + dp[i-k])
-        for (long long i = 0; i < 100001; i++){
+        for (int i = 0; i < 100001; i++){
             if (i == k){
                 dp.push_back(2);
             }else if (i < k){
@@ -24,21 +24,22 @@
                 dp.push_back((dp[i-1] + dp[i-k]) % 1000000007);
             }
         }
-        
-        for (long long i = 1; i < 100001; i++){
-            ps.push_back((ps[i-1] + dp[i-1]) % 1000000007);
+       
+        for (int i = 1; i < 100001; i++){
+            //ignoro o dp[0] pq ele representa o numero de maeiras de organizar uma sequencia de tamanho 0...
+            ps.push_back((ps[i-1] + dp[i]) % 1000000007);
         }
      
-        long long a, b;
-        long long soma;
-        for (long long i = 0; i < t; i++){
+        int a, b;
+        int soma;
+        for (int i = 0; i < t; i++){
             cin >> a;
             cin >> b;
-            
+           
             // ps entra em ação aqui
-            soma = ((ps[b+1] - ps[a]) + 1000000007) % 1000000007;
+            soma = ((ps[b] - ps[a-1]) + 1000000007) % 1000000007;
      
-            cout << soma << endl; 
+            cout << soma << endl;
         }
         return 0;
     }
